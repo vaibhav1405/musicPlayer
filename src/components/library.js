aae609b96cb46c19b023e2ef3b1fef0762e4ddbc
AryanShinde <aryan.shinde.29@gmail.com>
2021-08-31 23:27:28 +0530

    feat:skip

--- src/components/library.js
+++ src/components/library.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React from "react";
 import LibrarySong from "./librarySong";
 export default function  Library(props){
 
--- src/components/player.js
+++ src/components/player.js
@@ -1,4 +1,4 @@
-import React,{useRef,useState} from "react";
+import React,{useEffect, useRef,useState} from "react";
 import Library from "./library";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay,faBackward, faForward, faPause } from "@fortawesome/free-solid-svg-icons";
@@ -10,7 +10,18 @@ export default function Player(props){
         currentTime:0,
         duration:0
     });
-
+    useEffect(()=>{
+        const newSongs=props.songs.map(s=>{
+            if(s.id===props.currentSong.id){
+                return {...s,active:true};
+            }
+            else{
+                return {...s,active:false};
+            }
+        })
+        props.setSong(newSongs);
+    
+    },[props.currentSong]);
     function audioHandler(){
         if(props.flag){
         audioRef.current.play();
@@ -33,6 +44,25 @@ export default function Player(props){
         audioRef.current.currentTime=e.target.value;
 
     }
+    function SetDirection(direction){
+        let currentIndex=props.songs.findIndex((song)=>song.id===props.currentSong.id);
+        if(props.flag===false){
+            props.setFlag(true);
+        }
+        // let currentIndex=props.songs.indexOf(props.currentSong);
+        if(direction==="skip-forward"){
+            props.setCurrentSong(props.songs[(currentIndex+1)%props.songs.length]);
+        }
+        if(direction==="skip-backward"){
+            if(currentIndex===0){
+                props.setCurrentSong(props.songs[props.songs.length-1]);
+            }
+            else{
+            props.setCurrentSong(props.songs[(currentIndex-1)%props.songs.length]);
+            }
+        }
+        
+    }
 
     return(  
         <div className="player">
@@ -40,11 +70,12 @@ export default function Player(props){
                 <h4>{setCTime(songInfo.currentTime)}</h4>
                 <input type="range" onChange={setInput} value={songInfo.currentTime} min="0" max={songInfo.duration} />
                 <h4>{setCTime(songInfo.duration)}</h4>
+                <p>{songInfo.duration==null ? songInfo.duration="0:00":""}</p>
             </div>
             <div className="controllers">
-            <FontAwesomeIcon size="2x" icon={faBackward}/>
-            <FontAwesomeIcon onClick={audioHandler} size="2x" icon={ props.flag ? faPlay:faPause}/>
-            <FontAwesomeIcon size="2x" icon={faForward}/>
+            <FontAwesomeIcon size="2x" onClick={()=>SetDirection("skip-backward")} icon={faBackward}/>
+            <FontAwesomeIcon onClick={audioHandler}  size="2x" icon={ props.flag ? faPlay:faPause}/>
+            <FontAwesomeIcon size="2x" onClick={()=>SetDirection("skip-forward")} icon={faForward}/>
             </div>
             <audio onTimeUpdate={getTime} onLoadedMetadata={getTime} ref={audioRef} src={props.currentSong.audio}></audio>
             <Library libraryCheck={props.libraryCheck} setSong={props.setSong} flag={props.flag} audioRef={audioRef} setCurrentSong={props.setCurrentSong} currentSong={props.currentSong} setFlag={props.setFlag} songs={props.songs}/>
