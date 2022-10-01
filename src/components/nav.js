424c301d5f5d4a0190134ca771b2b4976895251e
Vaibhav Dadhich <V.dadhic@quadfi.com>
2022-10-01 15:16:38 +0530

    Feature:Dark mode implemented

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAtom } from "@fortawesome/free-solid-svg-icons";
+import { faAtom,faEye } from "@fortawesome/free-solid-svg-icons";
 
-export default function Nav({libraryCheck,setLibraryCheck}){
+export default function Nav({libraryCheck,setLibraryCheck , setTheme , theme}){
+    const toggleTheme = () => {
+        if (theme === 'light') {
+          setTheme('dark');
+        } else {
+          setTheme('light');
+        }
+      };
     return(
         <div className="nav"  >
             <h1>AryanS</h1>
             <button onClick={()=>{setLibraryCheck(!libraryCheck)}}><FontAwesomeIcon className="nav-burger" size="2x" icon={faAtom}/></button>
+            <button onClick={toggleTheme}><FontAwesomeIcon className="nav-burger" size="2x" icon={faEye}/></button>
         </div>
     );
 }
